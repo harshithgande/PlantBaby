716aba29f698bcf35df960c2ec3099e019168c2a
harshithgande <156864163+harshithgande@users.noreply.github.com>
2025-02-18 14:44:29 -0800

    Fixed routing for GitHub Pages

--- PlantBaby/src/components/Header.jsx
+++ PlantBaby/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <nav className="nav">
                 <div className="logo-container">
                     <Link to="/">
-                        <img src="https://files.oaiusercontent.com/file-Ktz38XbX9F2TP9Lc2a6Aoc?se=2025-02-08T00%3A04%3A18Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D299%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Dlogo.png&sig=a%2BRV2j/GiPhSM%2BntshckbNhGWLm7TGx860vtXvXc2Qk%3D" alt="PlantBaby Logo" className="logo-img" />
+                        <img src="logo.png" alt="PlantBaby Logo" className="logo-img" />
                     </Link>
                 </div>
                 <ul className="nav-links">
